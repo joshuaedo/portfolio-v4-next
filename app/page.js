90f3a01e59d4be20d2e6a7d4ba270da093298aa8
Joshua Edo <joshua.edo01@gmail.com>
2023-06-18 15:30:15 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,11 +2,18 @@ import { Arrow } from "./image";
 
 export default function Home() {
   return (
-    <main>
-      <section className="px-0 md:px-52 h-screen">
-        <div className="text-6xl md:text-9xl opacity-75">
-          <p>SOFTWARE</p>
-          <p>DEVELOPER</p>
+    <main className="px-0 md:px-52">
+      <section className="h-screen flex items-center justify-center">
+        <div className="">
+          <p className="text-5xl md:text-9xl opacity-75 font-medium">
+            SOFTWARE
+          </p>
+          <p className="ml-16 text-5xl md:text-9xl opacity-75 font-medium">
+            DEVELOPER
+          </p>
+          {/* <p className="text-xs md:text-lg px-[20%] opacity-100">
+            SPECIALIZED IN WEBSITE DESIGN & FRONTEND DEVELOPMENT
+          </p> */}
         </div>
       </section>
       <section className="h-screen">
