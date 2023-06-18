4a0f33b3f91783d3b4a908d87fcafd6c8cd4fc35
Joshua Edo <joshua.edo01@gmail.com>
2023-06-18 15:04:42 +0000

    Update 7 files

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,9 @@
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Cursor from "./components/cursor";
-// import localFont from "next/font/local/";
-// const sanFrancisco = localFont({
-//   src: "./fonts/SF-Pro.ttf",
-//   weight: ["100", "400", "900"],
-// });
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Joshua Edo • Software Developer",
@@ -15,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`font-sanFrancisco bg-black text-white tracking-wider`}>
+      <body className={`${inter.className} bg-black text-white tracking-wider`}>
         <Cursor />
         <Navbar />
         {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,6 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      fontFamily: {
-        sanFrancisco: ["sanFrancisco", "sans-serif"],
-      },
     },
   },
   plugins: [],
