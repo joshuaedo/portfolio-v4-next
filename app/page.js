f04dbdc36e1770083a51d849d5b3043088f8299f
Joshua Edo <joshua.edo01@gmail.com>
2023-06-19 08:01:35 +0000

    Update navbar.js and page.js

--- app/page.js
+++ app/page.js
@@ -2,18 +2,24 @@ import { Arrow } from "./image";
 
 export default function Home() {
   return (
-    <main className="px-0 md:px-52">
-      <section className="h-screen flex items-center justify-center">
-        <div className="">
-          <p className="text-5xl md:text-9xl opacity-75 font-medium">
+    <main className="p-4 md:px-52">
+      <section className="relative h-screen flex items-center justify-center font-medium">
+        <div className="text-center md:text-end">
+          <p className="mr-0 md:mr-16 text-5xl md:text-9xl drop-shadow-lg text-[#A3A3A3]">
             SOFTWARE
           </p>
-          <p className="ml-16 text-5xl md:text-9xl opacity-75 font-medium">
-            DEVELOPER
+          <p className="text-5xl md:text-9xl drop-shadow-lg text-[#A3A3A3]">
+            ENGINEER
           </p>
-          {/* <p className="text-xs md:text-lg px-[20%] opacity-100">
-            SPECIALIZED IN WEBSITE DESIGN & FRONTEND DEVELOPMENT
-          </p> */}
+          <p className="text-xs md:text-lg inline">
+            SPECIALIZED IN FRONTEND DEVELOPMENT
+          </p>
+        </div>
+        <div className="absolute bottom-10 flex items-center md:items-end">
+          <p className="text-xs md:text-lg flex max-w-sm">
+            AVAILABLE FOR FREELANCE WORK
+            <Arrow h="h-3" w="w-3 ml-1" color="white" />
+          </p>
         </div>
       </section>
       <section className="h-screen">
