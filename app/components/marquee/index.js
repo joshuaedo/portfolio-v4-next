1c8f52cba036401b88f82676db2d0349a65c4791
Joshua Edo <joshua.edo01@gmail.com>
2023-06-23 19:19:22 +0000

    marquee

--- app/components/marquee/index.js
+++ app/components/marquee/index.js
@@ -1,10 +1,53 @@
 "use client";
 import styles from "./style.module.css";
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
 export default function Marquee() {
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.25,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-500px",
+    });
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    if (xPercent < -100) {
+      xPercent = 0;
+    } else if (xPercent > 0) {
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+    requestAnimationFrame(animate);
+    xPercent += 0.3 * direction;
+  };
+
   return (
-    <>
-  
-    </>
+    <main className={styles.main}>
+      <div className={styles.sliderContainer}>
+        <div ref={slider} className={styles.slider}>
+          <p ref={firstText}>Edo • Joshua • Thomas • Aniekan • Akpan • =) • </p>
+          <p ref={secondText}>
+            Edo • Joshua • Thomas • Aniekan • Akpan • =) •{" "}
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,34 +1,21 @@
 import Marquee from "./components/marquee";
 import HeroSection from "./components/heroSection";
 import ProjectsSection from "./components/projectsSection";
-// import StackSection from "./components/stackSection";
-// import { Arrow } from "./image";
 
 export default function HomePage() {
   return (
     <>
-      <div className="data-scroll-container p-4">
-        <div className="md:px-52">
-          <section className="relative h-[70vh] md:h-[80vh] flex items-center justify-center font-medium mb-28 md:mb-36">
-            <HeroSection />
-          </section>
-        </div>
+      <div className="p-4">
+        <section className="md:px-52 relative h-[70vh] md:h-[80vh] flex items-center justify-center font-medium mb-28 md:mb-36">
+          <HeroSection />
+        </section>
 
-        <div className="md:px-52">
-          <section className="relative bg-black">
-            <ProjectsSection />
-          </section>
-
-          {/* <section
-        
-          className="flex items-center justify-center"
-        >
-          <StackSection />
-        </section> */}
-        </div>
-
-        <section>
+        <section className="font-medium mb-52 md:mb-72">
           <Marquee />
+        </section>
+
+        <section className="relative bg-black md:px-52">
+          <ProjectsSection />
         </section>
       </div>
     </>
