2a48146c0da0d1b6f74ec4a6cd807c8766176ff4
Joshua Edo <joshua.edo01@gmail.com>
2023-06-23 19:58:16 +0000

    Update index.js and layout.js

--- app/components/lenisScroller/index.js
+++ app/components/lenisScroller/index.js
@@ -7,9 +7,12 @@ export const LenisScroller = () => {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 2.5,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
-      smoothTouch: true,
+      easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+      direction: "vertical",
+      gestureDirection: "vertical",
+      smooth: true,
+      smoothTouch: false,
+      touchMultiplier: 2,
     });
 
     lenis.on("scroll", (e) => {
