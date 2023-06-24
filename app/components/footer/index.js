dd0f0b8893bbb8cd829e089542f5c926933c4e05
Joshua Edo <joshua.edo01@gmail.com>
2023-06-24 00:34:52 +0000

    Update index.js and style.module.css

--- app/components/footer/index.js
+++ app/components/footer/index.js
@@ -1,48 +1,77 @@
+"use client";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
-function getCopyrightYear() {
-  const currentYear = new Date().getFullYear();
-  return currentYear;
-}
+const useDate = () => {
+  const locale = "en";
+  const [today, setDate] = useState(new Date()); // Save the current date to be able to trigger an update
 
-function getCurrentTime() {
-  const currentDate = new Date();
-  const currentTime = currentDate.toLocaleTimeString([], {
-    hour: "2-digit",
-    minute: "2-digit",
+  useEffect(() => {
+    const timer = setInterval(() => {
+      // Creates an interval which will update the current data every minute
+      // This will trigger a rerender every component that uses the useDate hook.
+      setDate(new Date());
+    }, 60 * 1000);
+    return () => {
+      clearInterval(timer); // Return a funtion to clear the timer so that it will stop being called on unmount
+    };
+  }, []);
+
+  const day = today.toLocaleDateString(locale, { weekday: "long" });
+  const date = `${day}, ${today.getDate()} ${today.toLocaleDateString(locale, {
+    month: "long",
+  })}\n\n`;
+
+  const hour = today.getHours();
+  const wish = `Good ${
+    (hour < 12 && "Morning") || (hour < 17 && "Afternoon") || "Evening"
+  } `;
+
+  const time = today.toLocaleTimeString(locale, {
+    hour: "numeric",
+    hour12: true,
+    minute: "numeric",
   });
-  return currentTime;
-}
+
+  return {
+    date,
+    time,
+    wish,
+  };
+};
 
 export default function Footer() {
-  const copyright = `${getCopyrightYear()}©`;
-  const time = getCurrentTime();
+  const { date, time, wish } = useDate(); // Invoke the useDate hook
+
+  const currentTime = time || getCurrentTime(); // If useDate hook doesn't provide time, fallback to getCurrentTime
 
   return (
     <>
       <footer
-        className={`grid grid-cols-1 md:grid-cols-2 h-[15vh] w-full bg-black bg-opacity-50 p-4 md:p-10 space-y-3`}
+        className={`grid grid-cols-1 md:grid-cols-2 h-[12vh] w-full bg-black bg-opacity-50 p-4 md:p-10 font-medium text-sm space-y-2 md:space-y-0`}
       >
-        <div className={`flex cursor-pointer justify-evenly  text-lg`}>
-          <p>{copyright}</p>
-          <p>{time}</p>
-        </div>
         <div
-          className={`flex cursor-pointer justify-evenly text-center text-xs`}
+          className={`hidden md:flex cursor-pointer justify-evenly text-center`}
         >
+          <p>{date}</p> {/* Replace with date from useDate */}
+          <p>{currentTime}</p>{" "}
+          {/* Replace with time from useDate or fallback value */}
+          <p>{wish}</p> {/* Add the wish from useDate */}
+        </div>
+        <div className={`flex cursor-pointer justify-evenly text-center`}>
           <Link
             href="https://github.com/joshuaedo"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <p className="">GITHUB</p>
+            <p className="uppercase md:capitalize">github</p>
           </Link>
           <Link
             href="https://twitter.com/thegr8joshua"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <p className="">TWITTER</p>
+            <p className="uppercase md:capitalize">twitter</p>
           </Link>
 
           <Link
@@ -50,14 +79,14 @@ export default function Footer() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <p className="">LINKED-IN</p>
+            <p className="uppercase md:capitalize">linkedin</p>
           </Link>
           <Link
             href="joshua.edo01@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <p className="">EMAIL</p>
+            <p className="uppercase md:capitalize">email</p>
           </Link>
         </div>
       </footer>
